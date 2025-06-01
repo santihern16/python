925d038a76dd12a5ff86f55e47ed5bc337a25c0a
Santiago Hernandez <sehernandezc@sanmateo.edu.co>
2025-06-01 14:01:15 -0500

    Add conditional statements examples and implement loop iterations with break and continue

--- 01-inicio/condicionales.py
+++ 01-inicio/condicionales.py
@@ -10,3 +10,9 @@ else:
     print("El numero es negativo.")
 
 print("fin del condicional")
+
+print("\nCondicionales ternarios")
+
+edad = 17
+mensaje = "Es mayor de edad" if edad >=18 else "Es menor de edad"
+print(mensaje)
---
+++ 03-loops/loop_for.py
@@ -0,0 +1,42 @@
+from os import system
+system("cls")
+
+frutas = ["manzana", "banana", "cereza", "durazno", "kiwi"]
+
+# Iterar una lista
+for fruta in frutas:
+    print(fruta)
+
+# Iterar sobre cualquier cosa que sea iterable
+cadena = "santixo"
+for char in cadena:
+    print(char)
+
+# enumerate()
+# permite iterar sobre un objeto iterable y obtener el índice y el valor
+frutas = ["manzana", "banana", "cereza", "durazno", "kiwi"]
+for index, fruta in enumerate(frutas):
+    print(f"{index}: {fruta}")
+
+# bucles anidados
+letras = ["A", "B", "C"]
+numeros = [1, 2, 3]
+
+for letra in letras:
+    for num in numeros:
+        print(f"{letra}{num}")
+
+# break y continue
+print("\nbreak")
+
+animales = ["perro", "gato", "elefante", "jirafa", "león"]
+for index, animal in enumerate(animales):
+    print(animal)
+    if animal == 'elefante':
+        print("Se ha encontrado al elefante en el índice", index)
+        break
+
+print("\ncontinue")
+for index, animal in enumerate(animales):
+    if animal == 'jirafa': continue
+    print(animal)
